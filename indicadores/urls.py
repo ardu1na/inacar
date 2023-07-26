59c14223792ddca22f14a0769d3c4c15b86e2591
ardu1na <arduinadelbosque@gmail.com>
2023-07-26 12:01:26 -0300

    manejar creacion de ormulario de evaluacion para empleado cono y sin instancia de evaluacion

--- indicadores/urls.py
+++ indicadores/urls.py
@@ -4,6 +4,8 @@ from indicadores import views
 urlpatterns = [
     #Administrador
     path('evaluacion/<int:id>/',views.evaluacion, name="evaluacion"),
+    path('evaluacion/',views.evaluacion, name="evaluacion_nueva"),
+
     path('success/',views.success, name="success"),
     path('evaluaciones_empleado/',views.evaluaciones_empleado, name="evaluaciones_empleado"),
 
